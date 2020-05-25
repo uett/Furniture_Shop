2e931c0a4b2d20fda6df96fc51de74894b9166d2
uett <vasy251745@gmail.com>
2020-05-25 16:16:43 +0500

    небольшие правки в стилях, исправление закрытия меню в адаптивном режиме

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-
-let hamburger = document.querySelector(".header__button");
-let headerMenu = document.querySelector(".header__list");
-
-hamburger.addEventListener("click", function(){
-    headerMenu.classList.add("header__list--the-drop-down-menu");
-})
